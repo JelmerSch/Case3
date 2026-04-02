7863c585213d123eb5c25daacf187040fa200c57
BuSa159 <buurman.sander@gmail.com>
2026-04-02 13:26:40 +0200

    Update Streamlit_start.py

--- Streamlit_start.py
+++ Streamlit_start.py
@@ -1,1 +1,12 @@
+import streamlit as st
+
+st.set_page_config(layout="wide", page_title="vlucht data analyse")
+
+dashboard = st.Page("Startdash.py", title="Startpagina",           icon=":material/dashboard:")
+page_1    = st.Page("page_1.py",   title="vlucht info",           icon=":material/article:")
+page_2    = st.Page("page_2.py",   title="vlucht info 2",       icon=":material/article:")
+page_3    = st.Page("page_3.py",   title="vlucht info 3",   icon=":material/article:")
+
+pg = st.navigation([dashboard, page_1, page_2, page_3])
+pg.run()
 
