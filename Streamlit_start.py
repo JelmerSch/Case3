802846c56a7aded7fde36bdae0e2343709cb169c
JelmerSch <jelmerschaaphok@gmail.com>
2026-04-02 16:37:11 +0200

    Update Streamlit_start.py

--- Streamlit_start.py
+++ Streamlit_start.py
@@ -1,8 +1,6 @@
 import streamlit as st
-from Startdash import initialize_data
 
 st.set_page_config(layout="wide", page_title="vlucht data analyse")
-initialize_data()
 
 dashboard = st.Page("Startdash.py", title="Startpagina",    icon=":material/dashboard:")
 page_1    = st.Page("page_1.py",   title="vlucht info",     icon=":material/article:")
