fec93eb013e781ae4d1fba5701702689ee2fea99
JelmerSch <jelmerschaaphok@gmail.com>
2026-04-02 16:29:38 +0200

    Update Streamlit_start.py
    juiste start

--- Streamlit_start.py
+++ Streamlit_start.py
@@ -1,12 +1,13 @@
 import streamlit as st
+from Startdash import initialize_data
 
 st.set_page_config(layout="wide", page_title="vlucht data analyse")
+initialize_data()
 
-dashboard = st.Page("Startdash.py", title="Startpagina",           icon=":material/dashboard:")
-page_1    = st.Page("page_1.py",   title="vlucht info",           icon=":material/article:")
-page_2    = st.Page("page_2.py",   title="vlucht info 2",       icon=":material/article:")
+dashboard = st.Page("Startdash.py", title="Startpagina",    icon=":material/dashboard:")
+page_1    = st.Page("page_1.py",   title="vlucht info",     icon=":material/article:")
+page_2    = st.Page("page_2.py",   title="vlucht info 2",   icon=":material/article:")
 page_3    = st.Page("page_3.py",   title="vlucht info 3",   icon=":material/article:")
 
 pg = st.navigation([dashboard, page_1, page_2, page_3])
 pg.run()
-
