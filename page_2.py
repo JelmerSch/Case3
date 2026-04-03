4e22ccadf887b5d184b4d1d8656e914704807424
BuSa159 <buurman.sander@gmail.com>
2026-04-03 09:46:57 +0200

    update page 2

--- page_2.py
+++ page_2.py
@@ -1,7 +1,5 @@
-import pandas as pd
-import matplotlib.pyplot as plt
-import plotly.express as px
-!pip install plotly
+
+
 
 
 
@@ -31,5 +29,40 @@ fig = px.scatter_geo(
 )
 
 fig.show()
+
+st.divider()
+
+# Een staaf diagram van de 10 drukste luchthavens
+
+df_airports = pd.read_csv('airports-extended-clean.csv', sep=';', decimal=',')
+df_schedule = pd.read_csv('schedule_airport.csv')
+
+# 2 aantal vluchten per luchthaven
+flight_counts = df_schedule.groupby('Org/Des').size().reset_index(name='Aantal_Vluchten')
+
+# 3. koppelen aan vliegvelden
+df_hubs = pd.merge(flight_counts, df_airports, left_on='Org/Des', right_on='ICAO', how='inner')
+
+# 4. Top 10 drukste luchthavens
+top_10_hubs = df_hubs.nlargest(10, 'Aantal_Vluchten')   
+
+# 5. Staafdiagram maken
+fig = px.bar(
+    top_10_hubs, 
+    x='Aantal Vluchten', 
+    y='Name',            # De naam van het vliegveld op de y-as
+    orientation='h',      # Horizontaal diagram voor betere leesbaarheid van namen
+    title='Top 10 Drukste Luchthavens',
+    labels={'Name': 'Luchthaven', 'Aantal Vluchten': 'Totaal aantal vluchten'},
+    color='Aantal Vluchten',
+    color_continuous_scale='Viridis'
+)
+
+# Zorg dat de drukste bovenaan staat
+fig.update_layout(yaxis={'categoryorder':'total ascending'})
+
+# 6. Weergeven in Streamlit
+st.title("Luchtvaart Dashboard")
+st.plotly_chart(fig, use_container_width=True)
 
 
