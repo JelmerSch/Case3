0d5033eca53e6caff66add2249cb7647a2d40ed7
BuSa159 <buurman.sander@gmail.com>
2026-04-06 20:25:59 +0200

    Update page_2.py

--- page_2.py
+++ page_2.py
@@ -9,6 +9,8 @@ BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 st.write(os.listdir(BASE_DIR))  # toont alle bestanden in de map
 
 
+st.write(os.path.getsize(os.path.join(BASE_DIR, 'schedule_airport.csv')))
+
 # --- 1. DATA INLADEN ---
 # We laden de data één keer in voor de hele pagina
 df_airports = pd.read_csv(os.path.join(BASE_DIR, 'airports-extended-clean.csv'), sep=';', decimal=',')
