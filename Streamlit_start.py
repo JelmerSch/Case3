9f68c477049b95dc044164d3707139a2484db182
JelmerSch <jelmerschaaphok@gmail.com>
2026-04-07 11:16:11 +0200

    Update Streamlit_start.py
    pagina 4

--- Streamlit_start.py
+++ Streamlit_start.py
@@ -6,6 +6,7 @@ dashboard = st.Page("Startdash.py", title="Startpagina",    icon=":material/dash
 page_1    = st.Page("page_1.py",   title="vlucht info 1",     icon=":material/article:")
 page_2    = st.Page("page_2.py",   title="vlucht info 2",   icon=":material/article:")
 page_3    = st.Page("page_3.py",   title="vlucht info 3",   icon=":material/article:")
+page_4    = st.Page("page_4.py",   title="vlucht info 4",   icon=":material/article:")
 
-pg = st.navigation([dashboard, page_1, page_2, page_3])
+pg = st.navigation([dashboard, page_1, page_2, page_3, page_4])
 pg.run()
