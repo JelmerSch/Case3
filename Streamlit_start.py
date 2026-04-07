49176d92bc28ba008c72c03fe573dc6852f16588
JelmerSch <jelmerschaaphok@gmail.com>
2026-04-07 22:37:17 +0200

    Update Streamlit_start.py

--- Streamlit_start.py
+++ Streamlit_start.py
@@ -6,7 +6,7 @@ dashboard = st.Page("Startdash.py", title="Startpagina",                     ico
 page_1    = st.Page("page_2.py",   title="Wereldwijde Vluchtactiviteit",     icon=":material/article:")
 page_2    = st.Page("page_3.py",   title="Airport Operations",               icon=":material/article:")
 page_3    = st.Page("page_4.py",   title="Vertragingsanalyse",               icon=":material/article:")
-page_4    = st.Page("page_1.py",   title="Vliegtuig route",                  icon=":material/article:")
+page_4    = st.Page("page_1.py",   title="7 Vliegtuig vluchten",             icon=":material/article:")
 
 pg = st.navigation([dashboard, page_1, page_2, page_3, page_4])
 pg.run()
